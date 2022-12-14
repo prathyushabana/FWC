9cc0b1f682f0607e43351239bc88aa49a65f87dd
prathyushabana <118348655+prathyushabana@users.noreply.github.com>
2022-12-14 10:36:17 +0530

    Add files via upload

---
+++ assignment1_assembly/ass1.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd /home/student/Desktop
+avra jkflipflop.asm
+
+
+
+cd /home/student/Desktop
+pdflatex TtoD.tex
+xdg-open TtoD.pdf
+ 
+#cd /home/shashi/Avr-gcc
+#make clean
+#make
+
+
+
+
+
