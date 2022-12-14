a15f6ddfbf4ec0cd57854b05cf0b26531bb077d8
prathyushabana <118348655+prathyushabana@users.noreply.github.com>
2022-12-14 10:35:10 +0530

    Add files via upload

---
+++ assignment_1ide/src/main.cpp
@@ -0,0 +1,26 @@
+#include<Arduino.h>
+int J,K,Q,D,p,clk;
+void setup()
+{
+	pinMode(13,clk);
+	pinMode(7,OUTPUT);
+	pinMode(2,INPUT);
+	pinMode(3,INPUT);
+	pinMode(4,INPUT);
+	
+}
+void loop()
+{
+	digitalWrite(13,HIGH);
+	delay(1000);
+	Q=digitalRead(4);
+	J=digitalRead(2);
+	K=digitalRead(3);
+	D=J&&!Q || !K&&Q;
+	digitalWrite(7,D);
+	digitalWrite(13,LOW);
+	delay(1000);
+        p=D&&clk;
+
+
+}
