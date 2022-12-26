780eaf2046d884a60a796666ef4116c8606b7e60
prathyushabana <118348655+prathyushabana@users.noreply.github.com>
2022-12-26 17:14:08 +0530

    Add files via upload

---
+++ assignment1_assembly/codes/jkflipflop1.asm
@@ -0,0 +1,39 @@
+.include "/home/student/Desktop/assignment1_assembly/codes/m328Pdef.inc"
+
+ldi r16, 0b00100000 ;identifying output pin 5
+out DDRD,r16		;declaring pins as output
+
+
+
+ldi r16,0b00000001	;initializing j
+ldi r17,0b00000001	;initializing k
+ldi r18,0b00000001	;initializing q
+ldi r25,0b00000001      ;initializing p(clk)
+
+ldi r23,0b00000001
+eor r23,r18        ;q!
+AND r23,r16         ;g=j&&!q
+ldi r24,0b00000001 
+eor r24,r17        ;!k
+AND r24,r18        ;l=!k&&q
+OR r24,r23         ;a=(g||l)
+AND r25,r24        ;a&&p
+
+
+;following code is for displaying output
+;shifting LSB in r16 to 2nd position
+ldi r20, 0b00000010	;counter = 2
+
+rcall loopw		;calling the loopw routine
+
+out PORTD,r25		;writing output to pin 5
+
+
+Start:
+rjmp Start
+
+;loop for bit shifting
+loopw:	lsl r25		;left shift
+	dec r20			;counter --
+	brne loopw	;if counter != 0
+	ret
