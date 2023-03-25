addd20dbe6f6b2dd0b21923436ca9c508cc612ab
prathyushabana <118348655+prathyushabana@users.noreply.github.com>
2023-03-25 15:19:30 +0530

    Add files via upload

---
+++ vectors/12.10.3.6/codes/C_codes/lib.h
@@ -0,0 +1,29 @@
+double quadratic(double coefa,double coefb,double constant)
+{
+  double d,r1,r2;
+  double b;
+     d= (coefb*coefb) - (4*coefa*constant);
+     if (d>0)
+     {
+          r1 = -coefb+sqrt (d) / (2*coefa);
+          r2 = -coefb-sqrt (d) / (2*coefa);
+          printf ("The real roots = %lf,%lf \n", r1, r2);
+     }      
+     else if (d==0)
+     {
+          r1 = -coefb/(2*coefa);
+          r2 = -coefb/(2*coefa);
+          printf ("roots are equal =%lf, %lf \n", r1, r2);
+     }
+     else
+          printf("Roots are imaginary");
+        if(r1>0)
+        {
+          b=r1;
+        }
+        if(r2>0)
+        {
+          b=r2;
+        }
+        return b;  
+}
